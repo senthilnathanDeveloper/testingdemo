7e3d3ae64eaf4d906925b574a89bff2a07cf1545
senthil <senthilnathan14062000@gmail.com>
2023-08-02 11:03:12 +0530

    added some files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TestingDemo from './Components/TestingDemo';
 
 function App() {
   return (
     <div className="App">
       <h3>This is React App --deployed the Netlify app</h3>
+      <TestingDemo/>
     </div>
   );
 }
