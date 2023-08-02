bc7462847f01ac46b5dadbd2ce5c24fb1283cb5b
senthil <senthilnathan14062000@gmail.com>
2023-08-02 11:22:29 +0530

    clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import TestingDemo from './Components/TestingDemo';
 
 function App() {
   return (
     <div className="App">
       <h3>This is React App --deployed the Netlify appss</h3>
-      {/* <TestingDemo/> */}
     </div>
   );
 }
