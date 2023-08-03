cad232d0f8620144fd65816cc7a8c2841f6e960f
senthil <senthilnathan14062000@gmail.com>
2023-08-03 12:56:33 +0530

    added some file

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+
+
+const Header = ({addedItems}) => {
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container fluid>
+                    <Navbar.Brand href="#">Shopping</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success" size='' style={{ width: "100%" }} >Added ({addedItems.length}) Item</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
---
+++ src/Components/MainContent/MainContent.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Row } from 'react-bootstrap'
+import './style.css'
+
+const MainContent = ({ addedItems, setAddedItems }) => {
+    const [items, setItem] = useState([]);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products/")
+            .then((res) => res.json())
+            .then((data) => setItem(data));
+    }, []);
+
+
+    const toggleAddedStatus = (itemId) => {
+        if (addedItems.includes(itemId)) {
+            setAddedItems(addedItems.filter((id) => id !== itemId));
+        } else {
+            setAddedItems([...addedItems, itemId]);
+        }
+    };
+
+
+
+    return (
+        <>
+            <Row className='row d-flex'>
+                {items.map((item, index) => (
+                    <Col key={index} lg='4' className='mt-5'>
+                        <Card style={{ width: '100%' }} className='h-100'>
+                            <Card.Img variant='top' src={item.image} style={{ width: '30%' }} />
+                            <Card.Body className='flex-fill'>
+                                <Card.Title className='text-decoration-underline'>{item.category}</Card.Title>
+                                <Card.Text>
+                                    <strong>{item.title}</strong>
+                                </Card.Text>
+                                <p>{item.description}</p>
+                                <div className='row'>
+                                    <div className='col-lg-12 d-flex align-items-center justify-content-between'>
+                                        <span>Price: {item.price}</span>
+                                        <Button
+                                            variant={addedItems.includes(item.id) ? 'danger' : 'primary'}
+                                            onClick={() => toggleAddedStatus(item.id)} // Toggle the added status on button click
+                                        >
+                                            {addedItems.includes(item.id) ? 'CANCEL' : 'ADD'}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default MainContent
