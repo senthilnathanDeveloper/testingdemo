da45471bc430ae4b6efd6fe0747dfd3f4c65fdb1
senthil <senthilnathan14062000@gmail.com>
2023-08-21 17:19:51 +0530

    added style

---
+++ src/Components/Products/ImageModal.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import './style.css';
+
+const ImageModal = ({ show, handleClose, imageSrc }) => {
+    const [zoomedIn, setZoomedIn] = useState(false);
+
+    const handleImageClick = () => {
+        setZoomedIn(!zoomedIn);
+    };
+
+    useEffect(() => {
+        if (!show) {
+            setZoomedIn(false);
+        }
+    }, [show]);
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-sm">Image</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className={`image-container ${zoomedIn ? 'zoomed-in' : ''}`}>
+                        <img
+                            src={imageSrc}
+                            className="zoomable-image"
+                            alt="Zoomed In"
+                            onClick={handleImageClick}
+                        />
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+};
+
+export default ImageModal;
